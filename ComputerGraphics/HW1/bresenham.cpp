af214ab264eb10127ff289738267da9f2cb02dc8
JunHyoung-Lee <59410333+Reitbe@users.noreply.github.com>
2022-04-01 21:06:55 +0900

    Not complete

--- ComputerGraphics/HW1/bresenham.cpp
+++ ComputerGraphics/HW1/bresenham.cpp
@@ -1,43 +1,82 @@
 #include <GL/glew.h>
 #include <GL/glut.h>
 #include <stdlib.h>
-#include <vector>
-#include <cmath>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
 
+int startX = 90;
+int startY = 10;
+int endX = 10;
+int endY = 80;
 
-
-
-
+void discrimination(int n, int x, int y) {
+	switch (n)
+	{
+	case 1:
+		glVertex2i(x, y);
+		break;
+	case 2:
+		glVertex2i(x - 2 * (x - startX), y);
+		break;
+	case 3:
+		glVertex2i(x, y - 2 * (y - startY));
+		break;
+	case 4:
+		glVertex2i(x, y);
+		break;
+	}
+}
 void bresenham() { //기울기가 0<m<1일 때 작동한다.
 	glClear(GL_COLOR_BUFFER_BIT);
 	glBegin(GL_POINTS);
 
-	int startX = 10;
-	int startY = 10;
-	int endX = 80;
-	int endY = 20;
-	int W = endX - startX;
-	int H = endY - startY;
+	int temp;
+	int caseNumber;
+
+	if (startX < startX && startY < endY) { caseNumber = 1; } // 1
+	else if (startX > startX && startY < endY) { endX = endX + 2 * (startX - endX); caseNumber = 2; } // 3
+	else if (startX < startX && startY > endY) { endY = endY + 2 * (startY - endY); caseNumber = 3;} // 2
+	else {
+		temp = startX; startX = endX; endX = temp;
+		temp = startY; startY = endY; endY = temp;
+		caseNumber = 4;
+	} //4
+
 	int x = startX;
 	int y = startY;
-	int F = 2 * H - W; // F의 초기값. 중점과 직선과의 관계 파악
-	int disF1 = 2 * H; // 중단점보다 직선이 아래있다. y유지
-	int disF2 = 2 * (H - W); // 중단점보다 직선이 위에있다. y+1
+	int W = endX - startX;
+	int H = endY - startY;
+	int F;
 
-	for (x; x <= endX; ++x) {
-		glVertex2i(x, y); // 시작점 찍고
-		if (F < 0) {
-			F += disF1;
-		}
-		else {
-			++y;
-			F += disF2;
+	if (W >= H) { // 기울기가 0<m<1인 경우
+		F = 2 * H - W; 
+		while (x <= endX) {
+			glVertex2i(x, y);
+			//discrimination(caseNumber, x, y); // 시작점 찍고
+			if (F < 0) {
+				F += 2 * H;
+			}
+			else {
+				++y;
+				F += 2 * (H - W);
+			}
+			++x;
 		}
 	}
+	else { // 기울기가 1<m<무한인 경우
+		F = 2 * W - H;
+		while (y <= endY) {
+			glVertex2i(x, y);
+			//discrimination(caseNumber, x, y);
+			if (F < 0) {
+				F += 2 * W;
+			}
+			else {
+				++x;
+				F += 2 * (W - H);
+			}
+			++y;
+		}
+	}
+
 	glEnd();
 	glFlush();
 }
